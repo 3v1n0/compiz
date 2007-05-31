f813d20bcdb0fb25117d924e1018fac349f39875
Mike Dransfield <mike@blueroot.co.uk>
2007-05-31 23:48:35 +0100

    Tidy up and add copyright
    (bzr r0.326.5)

--- notification.c
+++ notification.c
@@ -1,3 +1,23 @@
+/*
+ * Notification plugin for compiz
+ *
+ * Copyright (C) 2007 Mike Dransfield (mike (at) blueroot.co.uk)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
 #include <libnotify/notify.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -34,10 +54,10 @@ typedef struct _NotifyDisplay {
 
 
 void
-notifyLogMessage (CompDisplay *d,
-		  char *component,
+notifyLogMessage (CompDisplay  *d,
+		  char         *component,
 		  CompLogLevel level,
-		  char *message)
+		  char         *message)
 {
     NotifyNotification *n;
     char               *logLevel, iconFile[256], *iconUri, *homeDir;
