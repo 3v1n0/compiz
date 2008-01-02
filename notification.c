8315a5e7feaef4e2c00418fd0f5a00f7de025fa5
Danny Baumann <dannybaumann@web.de>
2008-01-02 10:50:32 +0100

    Adjust header.
    (bzr r0.326.15)

--- notification.c
+++ notification.c
@@ -2,6 +2,8 @@
  * Notification plugin for compiz
  *
  * Copyright (C) 2007 Mike Dransfield (mike (at) blueroot.co.uk)
+ * Maintained by Danny Baumann <dannybaumann (at) web.de>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
