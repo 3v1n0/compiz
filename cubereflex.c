d6f6f518dfb759ab1a06205655efe3f15d7ad9da
Dennis Kasprzyk <onestone@opencompositing.org>
2008-04-09 03:21:45 +0200

    Added new cylinder deformation mode.
    (bzr r0.377.39)

--- cubereflex.c
+++ cubereflex.c
@@ -34,6 +34,9 @@
 static int displayPrivateIndex;
 static int cubeDisplayPrivateIndex;
 
+#define CUBEREFLEX_GRID_SIZE    100
+#define CUBEREFLEX_CAP_ELEMENTS 30
+
 typedef struct _CubereflexDisplay
 {
     int screenPrivateIndex;
@@ -46,14 +49,18 @@ typedef struct _CubereflexScreen
     PaintScreenProc            paintScreen;
     PreparePaintScreenProc     preparePaintScreen;
     PaintTransformedOutputProc paintTransformedOutput;
+    AddWindowGeometryProc      addWindowGeometry;
 
-    CubeClearTargetOutputProc clearTargetOutput;
-    CubeGetRotationProc	      getRotation;
-    CubeCheckOrientationProc  checkOrientation;
+    CubeClearTargetOutputProc   clearTargetOutput;
+    CubeGetRotationProc	        getRotation;
+    CubeCheckOrientationProc    checkOrientation;
+    CubeShouldPaintViewportProc shouldPaintViewport;
+    CubePaintTopProc	        paintTop;
+    CubePaintBottomProc	        paintBottom;
 
     Bool reflection;
     Bool first;
-    
+
     CompOutput *last;
 
     float yTrans;
@@ -61,6 +68,12 @@ typedef struct _CubereflexScreen
 
     float backVRotate;
     float vRot;
+
+    float deform;
+
+    Region tmpRegion;
+
+    GLfloat capFill[CUBEREFLEX_CAP_ELEMENTS * 12];
 } CubereflexScreen;
 
 #define GET_CUBEREFLEX_DISPLAY(d) \
@@ -181,6 +194,298 @@ cubereflexClearTargetOutput (CompScreen *s,
 	glCullFace (GL_FRONT);
 }
 
+static Bool
+cubereflexShouldPaintViewport (CompScreen              *s,
+			       const ScreenPaintAttrib *sAttrib,
+			       const CompTransform     *transform,
+			       CompOutput              *outputPtr,
+			       PaintOrder              order)
+{
+    Bool rv = FALSE;
+
+    CUBEREFLEX_SCREEN (s);
+    CUBE_SCREEN (s);
+
+    if (rs->deform > 0.0)
+    {
+	float z[3];
+	Bool  ftb1, ftb2, ftb3;
+
+	z[0] = cs->invert * cs->distance;
+	z[1] = z[0] + (0.25 / cs->distance);
+	z[2] = cs->invert * sqrt (0.25 + (cs->distance * cs->distance));
+
+	CompVector vPoints[3][3] = { { {.v = { -0.5,  0.0, z[0], 1.0 } },
+				       {.v = {  0.0,  0.5, z[1], 1.0 } },
+				       {.v = {  0.0,  0.0, z[1], 1.0 } } },
+				     { {.v = {  0.5,  0.0, z[0], 1.0 } },
+				       {.v = {  0.0, -0.5, z[1], 1.0 } },
+				       {.v = {  0.0,  0.0, z[1], 1.0 } } },
+				     { {.v = { -0.5,  0.0, z[2], 1.0 } },
+				       {.v = {  0.0,  0.5, z[2], 1.0 } },
+				       {.v = {  0.0,  0.0, z[2], 1.0 } } } };
+
+	ftb1 = (*cs->checkOrientation) (s, sAttrib, transform,
+					outputPtr, vPoints[0]);
+	ftb2 = (*cs->checkOrientation) (s, sAttrib, transform,
+					outputPtr, vPoints[1]);
+	ftb3 = (*cs->checkOrientation) (s, sAttrib, transform,
+					outputPtr, vPoints[2]);
+
+	return (order == FTB && (ftb1 || ftb2 || ftb3)) ||
+	       (order == BTF && (!ftb1 || !ftb2 || !ftb3));
+    }
+    else
+    {
+	UNWRAP (rs, cs, shouldPaintViewport);
+	rv = (*cs->shouldPaintViewport) (s, sAttrib, transform,
+					 outputPtr, order);
+	WRAP (rs, cs, shouldPaintViewport, cubereflexShouldPaintViewport);
+    }
+
+    return rv;
+}
+
+static void
+cubereflexPaintTop (CompScreen		  *s,
+		  const ScreenPaintAttrib *sAttrib,
+		  const CompTransform     *transform,
+		  CompOutput		  *output,
+		  int			  size)
+{
+    ScreenPaintAttrib sa;
+    CompTransform     sTransform;
+    int               i, opacity;
+    Bool              wasCulled = glIsEnabled (GL_CULL_FACE);
+
+    CUBE_SCREEN (s);
+    CUBEREFLEX_SCREEN (s);
+
+    UNWRAP (rs, cs, paintTop);
+    (*cs->paintTop) (s, sAttrib, transform, output, size);
+    WRAP (rs, cs, paintTop, cubereflexPaintTop);
+
+    if (rs->deform == 0.0 || !cubereflexGetFillCaps (s))
+	return;
+
+    for (i = 0; i < CUBEREFLEX_CAP_ELEMENTS * 4; i++)
+	rs->capFill[(i * 3) + 1] = 0.5;
+
+    opacity = (cs->desktopOpacity * cubereflexGetTopColor(s)[3]) / 0xffff;
+    glColor4us (cubereflexGetTopColor(s)[0],
+		cubereflexGetTopColor(s)[1],
+		cubereflexGetTopColor(s)[2],
+		opacity);
+
+    glPushMatrix ();
+
+    if (cs->desktopOpacity != OPAQUE)
+    {
+	glEnable (GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    glDisableClientState (GL_TEXTURE_COORD_ARRAY);
+    glVertexPointer (3, GL_FLOAT, 0, rs->capFill);
+
+    glDisable (GL_CULL_FACE);
+
+    for (i = 0; i < size; i++)
+    {
+	sa = *sAttrib;
+	sTransform = *transform;
+	sa.yRotate += (360.0f / size) * i;
+
+	(*s->applyScreenTransform) (s, &sa, output, &sTransform);
+
+        glLoadMatrixf (sTransform.m);
+
+	glDrawArrays (GL_TRIANGLE_FAN, 0, CUBEREFLEX_CAP_ELEMENTS + 2);
+    }
+    glEnableClientState (GL_TEXTURE_COORD_ARRAY);
+    glDisable (GL_BLEND);
+    glBlendFunc (GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+
+    if (wasCulled)
+	glEnable (GL_CULL_FACE);
+
+    glPopMatrix ();
+
+    glColor4usv (defaultColor);
+}
+
+/*
+ * Paint bottom cube face
+ */
+static void
+cubereflexPaintBottom (CompScreen		     *s,
+		      const ScreenPaintAttrib *sAttrib,
+		      const CompTransform     *transform,
+		      CompOutput		     *output,
+		      int		     size)
+{
+    ScreenPaintAttrib sa;
+    CompTransform     sTransform;
+    int               i, opacity;
+    Bool              wasCulled = glIsEnabled (GL_CULL_FACE);
+
+    CUBE_SCREEN (s);
+    CUBEREFLEX_SCREEN (s);
+
+    UNWRAP (rs, cs, paintBottom);
+    (*cs->paintBottom) (s, sAttrib, transform, output, size);
+    WRAP (rs, cs, paintBottom, cubereflexPaintBottom);
+
+    if (rs->deform == 0.0 || !cubereflexGetFillCaps (s))
+	return;
+
+    for (i = 0; i < CUBEREFLEX_CAP_ELEMENTS * 4; i++)
+	rs->capFill[(i * 3) + 1] = -0.5;
+
+    opacity = (cs->desktopOpacity * cubereflexGetBottomColor(s)[3]) / 0xffff;
+    glColor4us (cubereflexGetBottomColor(s)[0],
+		cubereflexGetBottomColor(s)[1],
+		cubereflexGetBottomColor(s)[2],
+		opacity);
+
+    glPushMatrix ();
+
+    if (cs->desktopOpacity != OPAQUE)
+    {
+	glEnable (GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    glDisableClientState (GL_TEXTURE_COORD_ARRAY);
+    glVertexPointer (3, GL_FLOAT, 0, rs->capFill);
+
+    glDisable (GL_CULL_FACE);
+
+    for (i = 0; i < size; i++)
+    {
+	sa = *sAttrib;
+	sTransform = *transform;
+	sa.yRotate += (360.0f / size) * i;
+
+	(*s->applyScreenTransform) (s, &sa, output, &sTransform);
+
+        glLoadMatrixf (sTransform.m);
+
+	glDrawArrays (GL_TRIANGLE_FAN, 0, CUBEREFLEX_CAP_ELEMENTS + 2);
+    }
+    glEnableClientState (GL_TEXTURE_COORD_ARRAY);
+    glDisable (GL_BLEND);
+    glBlendFunc (GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+
+    if (wasCulled)
+	glEnable (GL_CULL_FACE);
+
+    glPopMatrix ();
+
+    glColor4usv (defaultColor);
+}
+
+static void
+cubereflexAddWindowGeometry (CompWindow *w,
+			     CompMatrix *matrix,
+			     int	nMatrix,
+			     Region     region,
+			     Region     clip)
+{
+    CompScreen *s = w->screen;
+
+    CUBEREFLEX_SCREEN (s);
+    CUBE_SCREEN (s);
+
+    if (rs->deform > 0.0 && s->desktopWindowCount)
+    {
+	int         x1, x2, i, oldVCount = w->vCount;
+	REGION      reg;
+	GLfloat     *v;
+	int         offX = 0, offY = 0;
+	int         sx1, sx2, sw;
+
+	const float angle = atan (0.5 / cs->distance);
+	const float rad = 0.5 / sin (angle);
+			
+	reg.numRects = 1;
+	reg.rects = &reg.extents;
+
+	reg.extents.y1 = region->extents.y1;
+	reg.extents.y2 = region->extents.y2;
+	
+	x1 = (region->extents.x1 / CUBEREFLEX_GRID_SIZE) * CUBEREFLEX_GRID_SIZE;
+	x1 = region->extents.x1;
+	x2 = MIN (x1 + CUBEREFLEX_GRID_SIZE, region->extents.x2);
+	
+	UNWRAP (rs, s, addWindowGeometry);
+	while (x1 < region->extents.x2)
+	{
+	    reg.extents.x1 = x1;
+	    reg.extents.x2 = x2;
+
+	    XIntersectRegion (region, &reg, rs->tmpRegion);
+
+	    if (!XEmptyRegion (rs->tmpRegion))
+	    {
+		(*w->screen->addWindowGeometry) (w, matrix, nMatrix,
+						 rs->tmpRegion, clip);
+	    }
+
+	    x1 = x2;
+	    x2 = MIN (x2 + CUBEREFLEX_GRID_SIZE, region->extents.x2);
+	}
+	WRAP (rs, s, addWindowGeometry, cubereflexAddWindowGeometry);
+	
+	v  = w->vertices + (w->vertexStride - 3);
+	v += w->vertexStride * oldVCount;
+
+	if (!windowOnAllViewports (w))
+	{
+	    getWindowMovementForOffset (w, s->windowOffsetX,
+                                        s->windowOffsetY, &offX, &offY);
+	}
+
+	if (cs->moMode == CUBE_MOMODE_ONE)
+	{
+	    sx1 = 0;
+	    sx2 = s->width;
+	    sw  = s->width;
+	}
+	else if (cs->moMode == CUBE_MOMODE_MULTI)
+	{
+	    sx1 = rs->last->region.extents.x1;
+	    sx2 = rs->last->region.extents.x2;
+	    sw  = sx2 - sx1;
+	}
+	else
+	{
+	    sx1 = s->outputDev[cs->srcOutput].region.extents.x1;
+	    sx2 = s->outputDev[cs->srcOutput].region.extents.x2;
+	    sw  = sx2 - sx1;
+	}
+	
+	for (i = oldVCount; i < w->vCount; i++)
+	{
+	    if (v[0] + offX >= sx1 - CUBEREFLEX_GRID_SIZE &&
+		v[0] + offX < sx2 + CUBEREFLEX_GRID_SIZE)
+	    {
+		v[2] = (cos (angle - ((v[0] + offX - sx1) /
+		       (float)sw * angle * 2)) * rad) - cs->distance;
+		v[2] *= rs->deform;
+	    }
+
+	    v += w->vertexStride;
+	}
+    }
+    else
+    {
+	UNWRAP (rs, s, addWindowGeometry);
+	(*w->screen->addWindowGeometry) (w, matrix, nMatrix, region, clip);
+	WRAP (rs, s, addWindowGeometry, cubereflexAddWindowGeometry);
+    }
+}
+
 static void
 cubereflexPaintTransformedOutput (CompScreen              *s,
 				  const ScreenPaintAttrib *sAttrib,
@@ -191,11 +496,46 @@ cubereflexPaintTransformedOutput (CompScreen              *s,
 {
     static GLfloat light0Position[] = { -0.5f, 0.5f, -9.0f, 1.0f };
     CompTransform  sTransform = *transform;
+    float          x, progress;
 
     CUBEREFLEX_SCREEN (s);
     CUBE_SCREEN (s);
 
-    if (cs->invert == 1 && rs->first)
+    if (cubereflexGetCylinder (s) &&
+	(cs->rotationState == RotationManual ||
+	(cs->rotationState == RotationChange &&
+	!cubereflexGetCylinderManualOnly (s)) || rs->deform > 0.0))
+    {
+	const float angle = atan (0.5 / cs->distance);
+	const float rad = 0.5 / sin (angle);
+	float       z, *quad;
+	int         i;
+	
+	(*cs->getRotation) (s, &x, &x, &progress);
+	rs->deform = progress;
+
+	rs->capFill[0] = 0.0;
+	rs->capFill[2] = cs->distance;
+
+	for (i = 0; i <= CUBEREFLEX_CAP_ELEMENTS; i++)
+	{
+	    x = -0.5 + ((float)i / (float)CUBEREFLEX_CAP_ELEMENTS);
+
+	    z = ((cos (angle - ((x + 0.5) * angle * 2)) * rad) -
+		 cs->distance) * rs->deform;
+
+
+	    quad = &rs->capFill[(i + 1) * 3];
+	    quad[0] = x;
+	    quad[2] = cs->distance + z;
+	}
+    }
+    else
+    {
+	rs->deform = 0.0;
+    }
+
+    if (cs->invert == 1 && rs->first && cubereflexGetReflection (s))
     {
 	rs->first = FALSE;
 	rs->reflection = TRUE;
@@ -226,6 +566,7 @@ cubereflexPaintTransformedOutput (CompScreen              *s,
 	    float         rYTrans;
 	    CompVector    point  = { .v = { -0.5, -0.5, cs->distance, 1.0 } };
 	    CompVector    point2 = { .v = { -0.5,  0.5, cs->distance, 1.0 } };
+	    float         deform;
 
 	    (*cs->getRotation) (s, &xRot, &vRot, &p);
 
@@ -250,6 +591,10 @@ cubereflexPaintTransformedOutput (CompScreen              *s,
 
 	    if (xRotate2 >= angle / 2.0)
 		xRotate2 = angle - xRotate2;
+
+	    xRotate = (rs->deform * angle * 0.5) + ((1.0 - rs->deform) * xRotate);
+	    xRotate2 = (rs->deform * angle * 0.5) + ((1.0 - rs->deform) * xRotate2);
+
 
 	    matrixGetIdentity (&pTransform);
 	    matrixRotate (&pTransform, xRotate, 0.0f, 1.0f, 0.0f);
@@ -295,6 +640,11 @@ cubereflexPaintTransformedOutput (CompScreen              *s,
 	    if (cubereflexGetMode (s) == ModeAbove)
 		rs->zTrans = 0.0;
 
+	    deform = (sqrt (0.25 + (cs->distance * cs->distance)) -
+		     cs->distance) * -rs->deform;
+
+	    rs->zTrans = MIN (rs->zTrans, deform);
+
 	    if (cubereflexGetMode (s) == ModeAbove && rs->vRot > 0.0)
 	    {
 		rs->backVRotate = rs->vRot;
@@ -305,14 +655,15 @@ cubereflexPaintTransformedOutput (CompScreen              *s,
 		(*s->applyScreenTransform) (s, sAttrib, output, &pTransform);
 		point.x = point.y = 0.0;
 		point.z = -cs->distance;
+		point.z += deform;
 		point.w = 1.0;
 		matrixMultiplyVector (&point, &point, &pTransform);
-
+		
 		matrixTranslate (&rTransform, 0.0, 0.0, point.z);
 		matrixRotate (&rTransform, rs->vRot, 1.0, 0.0, 0.0);
 		matrixScale (&rTransform, 1.0, -1.0, 1.0);
 		matrixTranslate (&rTransform, 0.0, 1.0, 0.0);
-		matrixTranslate (&rTransform, 0.0, 0.0, -point.z);
+		matrixTranslate (&rTransform, 0.0, 0.0, -point.z + rs->zTrans);
 	    }
 	    else
 	    {
@@ -408,6 +759,12 @@ cubereflexPaintTransformedOutput (CompScreen              *s,
 	rs->reflection = FALSE;
     }
 
+    if (!cubereflexGetReflection (s))
+    {
+	rs->yTrans = 0.0;
+	rs->zTrans = (sqrt (0.25 + (cs->distance * cs->distance)) -
+		     cs->distance) * -rs->deform;
+    }
 
     matrixTranslate (&sTransform, 0.0, rs->yTrans, rs->zTrans);
 
@@ -518,13 +875,20 @@ cubereflexInitScreen (CompPlugin *p,
     rs->last       = NULL;
     rs->yTrans     = 0.0;
     rs->zTrans     = 0.0;
+    rs->tmpRegion  = XCreateRegion ();
+    rs->deform     = 0.0;
 
     WRAP (rs, s, paintTransformedOutput, cubereflexPaintTransformedOutput);
     WRAP (rs, s, paintOutput, cubereflexPaintOutput);
     WRAP (rs, s, donePaintScreen, cubereflexDonePaintScreen);
+    WRAP (rs, s, addWindowGeometry, cubereflexAddWindowGeometry);
+
     WRAP (rs, cs, clearTargetOutput, cubereflexClearTargetOutput);
     WRAP (rs, cs, getRotation, cubereflexGetRotation);
     WRAP (rs, cs, checkOrientation, cubereflexCheckOrientation);
+    WRAP (rs, cs, shouldPaintViewport, cubereflexShouldPaintViewport);
+    WRAP (rs, cs, paintTop, cubereflexPaintTop);
+    WRAP (rs, cs, paintBottom, cubereflexPaintBottom);
 
     return TRUE;
 }
@@ -536,13 +900,19 @@ cubereflexFiniScreen (CompPlugin *p,
     CUBEREFLEX_SCREEN (s);
     CUBE_SCREEN (s);
 
+    XDestroyRegion (rs->tmpRegion);
+
     UNWRAP (rs, s, paintTransformedOutput);
     UNWRAP (rs, s, paintOutput);
     UNWRAP (rs, s, donePaintScreen);
+    UNWRAP (rs, s, addWindowGeometry);
 
     UNWRAP (rs, cs, clearTargetOutput);
     UNWRAP (rs, cs, getRotation);
     UNWRAP (rs, cs, checkOrientation);
+    UNWRAP (rs, cs, shouldPaintViewport);
+    UNWRAP (rs, cs, paintTop);
+    UNWRAP (rs, cs, paintBottom);
 
     free (rs);
 }
