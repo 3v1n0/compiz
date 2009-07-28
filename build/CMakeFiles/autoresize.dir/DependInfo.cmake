94bbddad39dff36f0b19d67edbcb9cde83520dc6
Sam Spilsbury (none) <smspillaz@XPS-FEDORA.>
2009-07-28 21:58:02 +0800

    Revert "Remove backup and build files"
    This reverts commit d5e25c8e891c9d0a05e80c18cbb923fd8a7a587a.
    
    (bzr r0.64.41)

---
+++ build/CMakeFiles/autoresize.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/smspillaz/Source/compiz/dev/plugins/autoresize/src/autoresize.cpp" "/home/smspillaz/Source/compiz/dev/plugins/autoresize/build/CMakeFiles/autoresize.dir/src/autoresize.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
