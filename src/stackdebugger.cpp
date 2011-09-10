5dac031859e1c97a9e5a13694e84bbc24bec939b
Sam Spilsbury <sam.spilsbury@canonical.com>
2011-09-10 21:26:02 +0800

    Merge in StackDebugger stack sanity checker
    (bzr r2800.2.13)

--- src/stackdebugger.cpp
+++ src/stackdebugger.cpp
@@ -49,7 +49,8 @@ StackDebugger::SetDefault (StackDebugger *dbg)
 StackDebugger::StackDebugger (Display *dpy, Window root) :
     mServerNChildren (0),
     mServerChildren (NULL),
-    mHandledConfigureEvent (false),
+    mWindowsChanged (false),
+    mServerWindowsChanged (false),
     mRoot (root),
     mDpy (dpy)
 {
@@ -76,22 +77,7 @@ StackDebugger::loadStack ()
     XQueryTree (mDpy, mRoot, &rootRet, &parentRet,
 		&mServerChildren, &mServerNChildren);
 
-    mHandledConfigureEvent = false;
     mDestroyedFrames.clear ();
-}
-
-void
-StackDebugger::handleEvent (XEvent *event)
-{
-    switch (event->type)
-    {
-	case ConfigureNotify:
-	    if (event->xconfigure.above)
-		mHandledConfigureEvent = true;
-	    break;
-	default:
-	    break;
-    }
 }
 
 void
@@ -104,12 +90,6 @@ void
 StackDebugger::removeDestroyedFrame (Window f)
 {
     mDestroyedFrames.remove (f);
-}
-
-bool
-StackDebugger::stackChange ()
-{
-    return mHandledConfigureEvent;
 }
 
 bool
@@ -132,7 +112,7 @@ StackDebugger::cmpStack (CompWindowList &windows,
     }
 
     if (verbose)
-	fprintf (stderr, "sent       | recv       | server     |\n\n");
+	compLogMessage ("stackdebugger", CompLogLevelDebug, "sent       | recv       | server     |");
 
     for (;(lrrit != windows.rend () ||
 	   lsrit != serverWindows.rend () ||
@@ -152,16 +132,12 @@ StackDebugger::cmpStack (CompWindowList &windows,
 	    sXid = serverSideWindows[serverSideWindows.size () - (i + 1)];
 
 	if (verbose)
-	    fprintf (stderr, "id 0x%x id 0x%x id 0x%x",
+	    compLogMessage ("stackdebugger", CompLogLevelDebug, "id 0x%x id 0x%x id 0x%x %s",
 		     (unsigned int) lsXid, (unsigned int) lrXid,
-		     (unsigned int) sXid);
+		     (unsigned int) sXid, lrXid != sXid ? "  /!\\ " : "");
 
 	if (lrXid != sXid)
-	{
-	    if (verbose)
-		fprintf (stderr, "  /!\ \n");
 	    err = true;
-	}
 
 	if (lrrit != windows.rend ())
 	    lrrit++;
@@ -171,6 +147,250 @@ StackDebugger::cmpStack (CompWindowList &windows,
 
 	if (i != serverSideWindows.size ())
 	    i++;
+    }
+
+    return err;
+}
+
+/* Checks the sanity of the list of windows last sent to the server.
+ *
+ * There are a few stacking "layers" here. From top to bottom:
+ * - 1) Docks stacked above toplevel windows which are stacked
+ *      above fullscreen windows
+ * - 2) "Keep above" toplevel windows above fullscreen windows
+ *      where a toplevel is in focus
+ * - 3) Toplevel windows in focus above fullscreen windows
+ * - 4) Fullscreen windows
+ * - 5) Dock windows
+ * - 6) Keep above windows
+ * - 7) Toplevel windows
+ * - 8) Docks which are marked "Keep Below"
+ * - 9) "Keep Below" windows
+ * - 10) Desktop windows
+ *
+ * There are also a few rules which apply here:
+ * - 1) Dock windows should always be above normal windows
+ *      except if marked keep below on any layer.
+ * - 2) Dock windows should ONLY be on one layer at a time,
+ *      eg if they are on layer 1 then there cannot
+ *      also be dock windows on layer 5 (except in the
+ *      case of below dock windows on layer 8)
+ * - 3) Fullscreen windows must always be above docks when in
+ *      focus, no matter if there is another window with "Keep Above"
+ * - 4) Focused windows take priority over fullscreen windows and
+ *      docks must always be above them (see rule 1)
+ *
+ * As we pass through each layer, this function flags each one from
+ * lowest being the most bits set to highest being the least bits
+ * set. If a window violates this it raises a warning */
+
+#define DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN 0xffffffff >> 1
+#define KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN 0xffffffff >> 2
+#define TOPLEVELS_ABOVE_FULLSCREEN 0xffffffff >> 3
+#define FULLSCREEN 0xffffffff >> 4
+#define DOCKS 0xffffffff >> 5
+#define KEEP_ABOVE 0xffffffff >> 6
+#define TOPLEVELS 0xffffffff >> 7
+#define DOCKS_BELOW 0xffffffff >> 8
+#define KEEP_BELOW 0xffffffff >> 9
+#define DESKTOP 0xffffffff >> 10
+
+namespace
+{
+    bool setCurrentLayer (Window requestingWindow, int layer, int &current)
+    {
+	bool ret = false;
+	/* Only allow steps down */
+	if ((current & layer) != layer)
+	{
+	    ret = true;
+	    compLogMessage ("stackdebugger", CompLogLevelWarn, "0x%x requested invalid layer 0x%x",
+			    requestingWindow, layer, current);
+	}
+
+	current = layer;
+
+	return ret;
+    }
+}
+
+bool
+StackDebugger::checkSanity (CompWindowList &serverWindows, bool verbose)
+{
+    int current = 0xffffffff;
+    int oldCurrent = current;
+    bool err = false;
+
+    if (verbose)
+	compLogMessage ("stackdebugger", CompLogLevelDebug, "processing new stack --------");
+
+    /* go backwards down the stack */
+    for (CompWindowList::reverse_iterator rit = serverWindows.rbegin ();
+	 rit != serverWindows.rend (); rit++)
+    {
+	CompWindow *w = (*rit);
+
+	/* Override redirect windows set all kinds
+	 * of crazy stuff and are required to stack
+	 * themselves so skip those */
+	if (w->overrideRedirect ())
+	    continue;
+
+	/* ignore non-override redirect unmanaged windows */
+	if (!w->managed ())
+	    continue;
+
+	/* determine the current layer */
+	if (w->type () == CompWindowTypeDockMask)
+	{
+	    if ((current & DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN) ==
+			   DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN)
+	    {
+		bool fullscreenWindow = false;
+
+		/* search down the stack to check if there is a fullscreen
+		 * window, otherwise we are not on the fullscreen layer */
+		for (CompWindow *rw = w->serverPrev; rw; rw = rw->serverPrev)
+		{
+		    if (rw->type () & CompWindowTypeFullscreenMask)
+		    {
+			fullscreenWindow = true;
+			break;
+		    }
+		}
+
+		/* if there is no fullscreen window, change the layer */
+		if (!fullscreenWindow)
+		    err |= setCurrentLayer (w->id (), DOCKS, current);
+		else
+		    err |= setCurrentLayer (w->id (), DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN, current);
+	    }
+	    else if (w->state () & CompWindowStateBelowMask)
+		err |= setCurrentLayer (w->id (), DOCKS_BELOW, current);
+	    else
+		err |= setCurrentLayer (w->id (), DOCKS, current);
+	}
+	else if (w->type () == CompWindowTypeFullscreenMask)
+	{
+	    err |= setCurrentLayer (w->id (), FULLSCREEN, current);
+	}
+	else if (w->type () == CompWindowTypeDesktopMask)
+	{
+	    err |= setCurrentLayer (w->id (), DESKTOP, current);
+	}
+	/* everything else that is not a fullscreen window or a desktop */
+	else
+	{
+	    if (w->state () & CompWindowStateAboveMask)
+	    {
+		if ((current & KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN) ==
+			       KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN)
+		{
+		    bool fullscreenWindow = false;
+
+		    /* search down the stack to check if there is a fullscreen
+		     * window, otherwise we are not on the fullscreen layer */
+		    for (CompWindow *rw = w->serverPrev; rw; rw = rw->serverPrev)
+		    {
+			if (rw->type () == CompWindowTypeFullscreenMask)
+			{
+			    fullscreenWindow = true;
+			    break;
+			}
+		    }
+
+		    if (!fullscreenWindow)
+			err |= setCurrentLayer (w->id (), KEEP_ABOVE, current);
+		    else
+			err |= setCurrentLayer (w->id (), KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN, current);
+		}
+		else
+		    err |= setCurrentLayer (w->id (), KEEP_ABOVE, current);
+	    }
+	    else if (w->state () & CompWindowStateBelowMask)
+		err |= setCurrentLayer (w->id (), KEEP_BELOW, current);
+	    else
+	    {
+		if ((current & TOPLEVELS_ABOVE_FULLSCREEN) ==
+			       TOPLEVELS_ABOVE_FULLSCREEN)
+		{
+		    bool fullscreenWindow = false;
+
+		    /* search down the stack to check if there is a fullscreen
+		     * window, otherwise we are not on the fullscreen layer */
+		    for (CompWindow *rw = w->serverPrev; rw; rw = rw->serverPrev)
+		    {
+			if (rw->type () == CompWindowTypeFullscreenMask)
+			{
+			    fullscreenWindow = true;
+			    break;
+			}
+		    }
+
+		    if (!fullscreenWindow)
+			err |= setCurrentLayer (w->id (), TOPLEVELS, current);
+		    else
+			err |= setCurrentLayer (w->id (), TOPLEVELS_ABOVE_FULLSCREEN, current);
+		}
+		else
+		    err |= setCurrentLayer (w->id (), TOPLEVELS, current);
+	    }
+	}
+
+	if ((current & DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN) ==
+		       DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer DOCKS_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN");
+	}
+	else if ((current & KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN) ==
+			    KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer KEEP_ABOVE_TOPLEVELS_ABOVE_FULLSCREEN");
+	}
+	else if ((current & TOPLEVELS_ABOVE_FULLSCREEN) == TOPLEVELS_ABOVE_FULLSCREEN)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer TOPLEVELS_ABOVE_FULLSCREEN");
+	}
+	else if ((current & FULLSCREEN) == FULLSCREEN)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer FULLSCREEN");
+	}
+	else if ((current & DOCKS) == DOCKS)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer DOCKS");
+	}
+	else if ((current & KEEP_ABOVE) == KEEP_ABOVE)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer KEEP_ABOVE");
+	}
+	else if ((current & TOPLEVELS) == TOPLEVELS)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer TOPLEVELS");
+	}
+	else if ((current & DOCKS_BELOW) == DOCKS_BELOW)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer DOCKS_BELOW");
+	}
+	else if ((current & KEEP_BELOW) == KEEP_BELOW)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer KEEP_BELOW");
+	}
+	else if ((current & DESKTOP) == DESKTOP)
+	{
+	    if (verbose && current != oldCurrent)
+		compLogMessage ("stackdebugger", CompLogLevelDebug, "on layer DESKTOP");
+	}
+
+	oldCurrent = current;
     }
 
     return err;
