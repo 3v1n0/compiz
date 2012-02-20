05c877151f1526e43b9ee827fd8abb81afa9b265
smspillaz <sam.spilsbury@canonical.com>
2012-02-20 15:51:26 +0800

    Check if the mutter key exists before using it
    (bzr r3009.3.2)

--- gtk/window-decorator/settings.c
+++ gtk/window-decorator/settings.c
@@ -147,12 +147,18 @@ use_tooltips_changed (GConfClient *client)
 static gboolean
 mutter_draggable_border_width_changed (GConfClient *client)
 {
+    static const unsigned int default_draggable_border_width = 7;
     int      new_width;
     int      width = settings->mutter_draggable_border_width;
 
-    new_width = gconf_client_get_int (client,
-				      MUTTER_DRAGGABLE_BORDER_WIDTH_KEY,
-				      NULL);
+    if (gconf_client_dir_exists (client, "/apps/mutter/general", NULL))
+    {
+	new_width = gconf_client_get_int (client,
+				          MUTTER_DRAGGABLE_BORDER_WIDTH_KEY,
+				          NULL);
+    }
+    else
+	new_width = default_draggable_border_width;
 
     if (new_width != width)
     {
