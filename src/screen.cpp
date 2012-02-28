8dc2b2b7d6da23c045e5c24c816d37c843759e24
Alan Griffiths <alan@octopull.co.uk>
2012-02-28 14:47:50 +0000

    Zero init the current (first) history element
    (bzr r3025.2.4)

--- src/screen.cpp
+++ src/screen.cpp
@@ -5006,7 +5006,7 @@ PrivateScreen::PrivateScreen (CompScreen *screen) :
 	boost::bind (&PrivateScreen::handleStartupSequenceTimeout, this));
     startupSequenceTimer.setTimes (1000, 1500);
 
-    memset (history[0].id, 0, sizeof (history[0].id));
+    memset (history[currentHistory], 0, sizeof history[currentHistory]);
 }
 
 cps::WindowManager::WindowManager() :
