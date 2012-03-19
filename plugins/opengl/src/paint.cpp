283f08610b0eefbe1e9a0dd8b8ccf9afcc007519
Daniel van Vugt <daniel.van.vugt@canonical.com>
2012-03-19 10:45:43 +0800

    Revert the fix for LP: #946887. It's obviously an unsafe optimization and has caused a regression (segfault) in some cases. (LP: #958540)
    Fixes LP: #958540
    
    (bzr r3058.2.1)

--- plugins/opengl/src/paint.cpp
+++ plugins/opengl/src/paint.cpp
@@ -217,6 +217,9 @@ PrivateGLScreen::paintOutputRegion (const GLMatrix   &transform,
     GLMatrix      vTransform;
     CompPoint     offXY;
 
+    CompWindowList                   pl;
+    CompWindowList::reverse_iterator rit;
+
     unredirectFS = CompositeScreen::get (screen)->
 	getOption ("unredirect_fullscreen_windows")->value ().b ();
 
@@ -231,8 +234,7 @@ PrivateGLScreen::paintOutputRegion (const GLMatrix   &transform,
 	count	       = 0;
     }
 
-    const CompWindowList &pl = cScreen->getWindowPaintList ();
-    CompWindowList::const_reverse_iterator rit;
+    pl = cScreen->getWindowPaintList ();
 
     if (!(mask & PAINT_SCREEN_NO_OCCLUSION_DETECTION_MASK))
     {
