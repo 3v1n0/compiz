d3e812b2bed46b3a4a55131fed66048ac86c4f58
Daniel van Vugt <daniel.van.vugt@canonical.com>
2012-04-18 18:26:36 +0800

    Don't refuse to fullscreen windows that have the same dimensions as the screen itself (LP: #946388)
    It seems that fitting a semi-maximized window is quite different to fitting
    a fullscreen window, because a fullscreen window does not have borders.
    
    Fixes LP: #946388
    
    (bzr r3094.1.1)

--- src/window.cpp
+++ src/window.cpp
@@ -641,26 +641,29 @@ CompWindow::recalcActions ()
      * the screen */
     bool foundVert = false;
     bool foundHorz = false;
+    bool foundFull = false;
 
     foreach (CompOutput &o, screen->outputDevs ())
     {
-	if (o.width () > (priv->sizeHints.min_width + priv->border.left + priv->border.right))
+	if (o.width () >= (priv->sizeHints.min_width + priv->border.left + priv->border.right))
 	    foundHorz = true;
-	if (o.height () > (priv->sizeHints.min_height + priv->border.top + priv->border.bottom))
+	if (o.height () >= (priv->sizeHints.min_height + priv->border.top + priv->border.bottom))
 	    foundVert = true;
+
+	/* Fullscreen windows don't need to fit borders... */
+	if (o.width () >= priv->sizeHints.min_width &&
+	    o.height () >= priv->sizeHints.min_height)
+	    foundFull = true;
     }
 
     if (!foundHorz)
-    {
-	actions &= ~(CompWindowActionMaximizeHorzMask |
-		     CompWindowActionFullscreenMask);
-    }
+	actions &= ~CompWindowActionMaximizeHorzMask;
 
     if (!foundVert)
-    {
-	actions &= ~(CompWindowActionMaximizeVertMask |
-		     CompWindowActionFullscreenMask);
-    }
+	actions &= ~CompWindowActionMaximizeVertMask;
+
+    if (!foundFull)
+	actions &= ~CompWindowActionFullscreenMask;
 
     if (!(priv->mwmFunc & MwmFuncAll))
     {
