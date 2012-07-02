c1623312a936031c243f57aa44caea62dc4e7aee
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-07-02 15:13:40 +0800

    Don't include compiz_discover_gtest_tests.cpp inline anymore
    (bzr r3270.2.3)

--- cmake/CompizCommon.cmake
+++ cmake/CompizCommon.cmake
@@ -70,123 +70,8 @@ elseif (IS_DIRECTORY ${CMAKE_SOURCE_DIR}/.bzr)
     set(IS_BZR_REPO 0)
 endif (IS_DIRECTORY ${CMAKE_SOURCE_DIR}/.bzr)
 
-# Create compiz_discover_gtest_tests target
-
-set (COMPIZ_DISCOVER_GTEST_TARGETS_SRC
-"#include <map>
-#include <vector>
-#include <string>
-#include <istream>
-#include <ostream>
-#include <fstream>
-#include <iterator>
-#include <iostream>
-#include <libgen.h>
-
-int main (int argc, char **argv)
-{
-    std::cin >> std::noskipws\;
-
-    if (argc < 2)
-    {
-	std::cout << \"Usage: PATH_TO_TEST_BINARY --gtest_list_tests | ./build_test_cases PATH_TO_TEST_BINARY\"\;
-	return 1\;
-    }
-
-    std::map<std::string, std::vector<std::string> > testCases\;
-    std::string line\;
-    std::string currentTestCase\;
-
-    while (std::getline (std::cin, line))
-    {
-	/* Is test case */
-	if (line.find (\"  \") == 0)
-	    testCases[currentTestCase].push_back (currentTestCase + line.substr (2))\;
-	else
-	    currentTestCase = line\;
-
-    }
-
-    std::ofstream testfilecmake\;
-    char *base = basename (argv[1])\;
-    std::string   gtestName (base)\;
-
-    testfilecmake.open (std::string (gtestName  + \"_test.cmake\").c_str (), std::ios::out | std::ios::trunc)\;
-
-    if (testfilecmake.is_open ())
-    {
-	for (std::map <std::string, std::vector<std::string> >::iterator it = testCases.begin ()\;
-	     it != testCases.end ()\; it++)
-	{
-	    for (std::vector <std::string>::iterator jt = it->second.begin ()\;
-		 jt != it->second.end ()\; jt++)
-	    {
-		if (testfilecmake.good ())
-		{
-		    std::string addTest (\"ADD_TEST (\")\;
-		    std::string testExec (\" \\\"\" + std::string (argv[1]) + \"\\\"\")\;
-		    std::string gTestFilter (\"\\\"--gtest_filter=\\\"\")\;
-		    std::string filterBegin (\"\\\"\")\;
-		    std::string filterEnd (\"\\\")\")\;
-
-		    testfilecmake << addTest << *jt << testExec << gTestFilter << filterBegin << *jt << filterEnd << std::endl\;
-		}
-	    }
-	}
-
-	testfilecmake.close ()\;
-    }
-
-    std::ifstream CTestTestfile (\"CTestTestfile.cmake\", std::ifstream::in)\;
-    bool needsInclude = true\;
-    line.clear ()\;
-
-    std::string includeLine = std::string (\"INCLUDE (\") +
-			      gtestName  +
-			      std::string (\"_test.cmake)\")\;
-
-    if (CTestTestfile.is_open ())
-    {
-	while (CTestTestfile.good ())
-	{
-	    std::getline (CTestTestfile, line)\;
-
-	    if (line == includeLine)
-		needsInclude = false\;
-	}
-
-	CTestTestfile.close ()\;
-    }
-
-    if (needsInclude)
-    {
-	std::ofstream CTestTestfileW (\"CTestTestfile.cmake\", std::ofstream::app | std::ofstream::out)\;
-
-	if (CTestTestfileW.is_open ())
-	{
-	    CTestTestfileW << includeLine\;
-	    CTestTestfileW.close ()\;
-	}
-    }
-
-    return 0\;
-}")
-
+# Create target to discover tests
 function (compiz_discover_tests EXECUTABLE)
-    if (NOT COMPIZ_DISCOVER_TESTS_GENERATED)
-	# We really should be using add_custom_command here
-	# but in reality GNU Make ensures that this is just
-	# another whole world of hurt
-	file (WRITE ${CMAKE_BINARY_DIR}/compiz_discover_gtest_tests.cpp ${COMPIZ_DISCOVER_GTEST_TARGETS_SRC})
-
-	add_executable (compiz_discover_gtest_tests
-			EXCLUDE_FROM_ALL
-			${CMAKE_BINARY_DIR}/compiz_discover_gtest_tests.cpp)
-	set (COMPIZ_DISCOVER_TESTS_GENERATED TRUE)
-
-	set_target_properties (compiz_discover_gtest_tests PROPERTIES
-			       RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-    endif (NOT COMPIZ_DISCOVER_TESTS_GENERATED)
 
     add_custom_command (OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/test_${EXECUTABLE}_wrapper.cpp
 			COMMAND echo "int main (void) { return 0; }" >> ${CMAKE_CURRENT_BINARY_DIR}/test_${EXECUTABLE}_wrapper.cpp
@@ -206,7 +91,7 @@ function (compiz_discover_tests EXECUTABLE)
 
     add_custom_command (TARGET ${EXECUTABLE}
 			POST_BUILD
-			COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/compiz_discover_gtest_tests ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE}
+			COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/compiz_gtest/compiz_discover_gtest_tests ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE}
 			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 			COMMENT "Discovering Tests in ${EXECUTABLE}"
 			DEPENDS 
