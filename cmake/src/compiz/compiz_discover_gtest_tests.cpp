9c0c1bc1d75ebe137eba4e25399374cc3d93db43
smspillaz <sam.spilsbury@canonical.com>
2012-07-03 02:45:41 -0700

    using namespace std
    (bzr r3270.2.5)

--- cmake/src/compiz/compiz_discover_gtest_tests.cpp
+++ cmake/src/compiz/compiz_discover_gtest_tests.cpp
@@ -8,21 +8,23 @@
 #include <iostream>
 #include <libgen.h>
 
+using namespace std;
+
 int main (int argc, char **argv)
 {
-    std::cin >> std::noskipws;
+    cin >> noskipws;
 
     if (argc < 2)
     {
-	std::cout << "Usage: PATH_TO_TEST_BINARY --gtest_list_tests | ./build_test_cases PATH_TO_TEST_BINARY";
+	cout << "Usage: PATH_TO_TEST_BINARY --gtest_list_tests | ./build_test_cases PATH_TO_TEST_BINARY";
 	return 1;
     }
 
-    std::map<std::string, std::vector<std::string> > testCases;
-    std::string line;
-    std::string currentTestCase;
+    map<string, vector<string> > testCases;
+    string line;
+    string currentTestCase;
 
-    while (std::getline (std::cin, line))
+    while (getline (cin, line))
     {
 	/* Is test case */
 	if (line.find ("  ") == 0)
@@ -32,29 +34,29 @@ int main (int argc, char **argv)
 
     }
 
-    std::ofstream testfilecmake;
+    ofstream testfilecmake;
     char *base = basename (argv[1]);
-    std::string   gtestName (base);
+    string   gtestName (base);
 
-    testfilecmake.open (std::string (gtestName  + "_test.cmake").c_str (), std::ios::out | std::ios::trunc);
+    testfilecmake.open (string (gtestName  + "_test.cmake").c_str (), ios::out | ios::trunc);
 
     if (testfilecmake.is_open ())
     {
-	for (std::map <std::string, std::vector<std::string> >::iterator it = testCases.begin ();
+	for (map <string, vector<string> >::iterator it = testCases.begin ();
 	     it != testCases.end (); it++)
 	{
-	    for (std::vector <std::string>::iterator jt = it->second.begin ();
+	    for (vector <string>::iterator jt = it->second.begin ();
 		 jt != it->second.end (); jt++)
 	    {
 		if (testfilecmake.good ())
 		{
-		    std::string addTest ("ADD_TEST (");
-		    std::string testExec (" \"" + std::string (argv[1]) + "\"");
-		    std::string gTestFilter ("\"--gtest_filter=\"");
-		    std::string filterBegin ("\"");
-		    std::string filterEnd ("\")");
+		    string addTest ("ADD_TEST (");
+		    string testExec (" \"" + string (argv[1]) + "\"");
+		    string gTestFilter ("\"--gtest_filter=\"");
+		    string filterBegin ("\"");
+		    string filterEnd ("\")");
 
-		    testfilecmake << addTest << *jt << testExec << gTestFilter << filterBegin << *jt << filterEnd << std::endl;
+		    testfilecmake << addTest << *jt << testExec << gTestFilter << filterBegin << *jt << filterEnd << endl;
 		}
 	    }
 	}
@@ -62,19 +64,19 @@ int main (int argc, char **argv)
 	testfilecmake.close ();
     }
 
-    std::ifstream CTestTestfile ("CTestTestfile.cmake", std::ifstream::in);
+    ifstream CTestTestfile ("CTestTestfile.cmake", ifstream::in);
     bool needsInclude = true;
     line.clear ();
 
-    std::string includeLine = std::string ("INCLUDE (") +
+    string includeLine = string ("INCLUDE (") +
 			      gtestName  +
-			      std::string ("_test.cmake)");
+			      string ("_test.cmake)");
 
     if (CTestTestfile.is_open ())
     {
 	while (CTestTestfile.good ())
 	{
-	    std::getline (CTestTestfile, line);
+	    getline (CTestTestfile, line);
 
 	    if (line == includeLine)
 		needsInclude = false;
@@ -85,11 +87,11 @@ int main (int argc, char **argv)
 
     if (needsInclude)
     {
-	std::ofstream CTestTestfileW ("CTestTestfile.cmake", std::ofstream::app | std::ofstream::out);
+	ofstream CTestTestfileW ("CTestTestfile.cmake", ofstream::app | ofstream::out);
 
 	if (CTestTestfileW.is_open ())
 	{
-	    CTestTestfileW << includeLine << std::endl;
+	    CTestTestfileW << includeLine << endl;
 	    CTestTestfileW.close ();
 	}
     }
