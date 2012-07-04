513126b1119adee4fdd1e1b76620e0fd21303899
smspillaz <sam.spilsbury@canonical.com>
2012-07-03 23:02:28 -0700

    Mocks for GLBufferBlitInterface
    (bzr r3201.6.9)

--- plugins/opengl/src/bufferblit/src/buffer-blit.cpp
+++ plugins/opengl/src/bufferblit/src/buffer-blit.cpp
@@ -1,1 +1,10 @@
 #include <opengl/bufferblit.h>
+
+using namespace compiz::opengl;
+
+void
+compiz::opengl::blitBuffers (unsigned int flags,
+			     const CompRegion &tmpRegion,
+			     GLBufferBlitInterface &blit)
+{
+}
--- plugins/opengl/src/bufferblit/tests/test-opengl-buffer-blit.cpp
+++ plugins/opengl/src/bufferblit/tests/test-opengl-buffer-blit.cpp
@@ -1,9 +1,24 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <opengl/bufferblit.h>
+
+using namespace compiz::opengl;
+using testing::_;
+
 class CompizOpenGLBufferBlitTest :
     public ::testing::Test
 {
+};
+
+class MockGLBufferBlit :
+    public GLBufferBlitInterface
+{
+    public:
+
+	MOCK_METHOD0 (swapBuffers, void ());
+	MOCK_METHOD0 (subBufferBlitAvailable, bool ());
+	MOCK_METHOD1 (subBufferBlit, void (const CompRegion &));
 };
 
 TEST(CompizOpenGLBufferBlitTest, TestTest)
