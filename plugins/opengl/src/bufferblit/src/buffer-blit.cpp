d2ed00e0f8f3e133d08ad3b0a6f4c842fea6ca53
smspillaz <sam.spilsbury@canonical.com>
2012-07-03 22:44:59 -0700

    Added fixture for buffer blit
    (bzr r3201.6.8)

---
+++ plugins/opengl/src/bufferblit/src/buffer-blit.cpp
@@ -0,0 +1,1 @@
+#include <opengl/bufferblit.h>
---
+++ plugins/opengl/src/bufferblit/tests/test-opengl-buffer-blit.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class CompizOpenGLBufferBlitTest :
+    public ::testing::Test
+{
+};
+
+TEST(CompizOpenGLBufferBlitTest, TestTest)
+{
+}
