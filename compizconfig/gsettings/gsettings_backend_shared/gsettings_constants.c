a8c98396af3acbb00f40f39830a6748ad629a558
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-07-06 19:06:33 +0800

    Change the default paths to fall in line with gsettings requirements
    (bzr r3262.3.57)

--- compizconfig/gsettings/gsettings_backend_shared/gsettings_constants.c
+++ compizconfig/gsettings/gsettings_backend_shared/gsettings_constants.c
@@ -3,10 +3,10 @@
 const char * const COMPIZ_SCHEMA_ID =  "org.freedesktop.compiz";
 const char * const COMPIZCONFIG_SCHEMA_ID = "org.freedesktop.compizconfig";
 const char * const PROFILE_SCHEMA_ID = "org.freedesktop.compizconfig.profile";
-const char * const COMPIZ       = "/apps/compiz-1";
-const char * const COMPIZ_PROFILEPATH = "/apps/compiz-1/profiles";
+const char * const COMPIZ       = "/org/freedesktop/compiz";
+const char * const COMPIZ_PROFILEPATH = "/org/freedesktop/compiz";
 const char * const COMPIZCONFIG = "/org/freedesktop/compizconfig";
-const char * constPROFILEPATH  = "/org/freedesktop/compizconfig/profiles";
+const char * const PROFILEPATH  = "/org/freedesktop/compizconfig/profiles";
 const char * const DEFAULTPROF = "Default";
 const char * const CORE_NAME   = "core";
 
