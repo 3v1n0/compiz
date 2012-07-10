a937872de47a4084a7b31f3a524daab62e733e77
Daniel van Vugt <daniel.van.vugt@canonical.com>
2012-07-10 14:52:13 +0800

    Clean up paths: /org/freedesktop/compizconfig/ -> /org/freedesktop/compiz/
    (bzr r3262.3.70)

--- compizconfig/gsettings/gsettings_backend_shared/gsettings_constants.c
+++ compizconfig/gsettings/gsettings_backend_shared/gsettings_constants.c
@@ -4,7 +4,7 @@ const char * const COMPIZ_SCHEMA_ID =  "org.freedesktop.compiz";
 const char * const COMPIZCONFIG_SCHEMA_ID = "org.freedesktop.compizconfig";
 const char * const PROFILE_SCHEMA_ID = "org.freedesktop.compizconfig.profile";
 const char * const COMPIZ       = "/org/freedesktop/compiz";
-const char * const COMPIZ_PROFILEPATH = "/org/freedesktop/compiz";
+const char * const COMPIZ_PROFILEPATH = "/org/freedesktop/compiz/";
 const char * const DEFAULTPROF = "Default";
 const char * const CORE_NAME   = "core";
 
