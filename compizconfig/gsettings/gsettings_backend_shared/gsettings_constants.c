61dcfd4258833df86233572671801e4011728030
Daniel van Vugt <daniel.van.vugt@canonical.com>
2012-07-10 15:29:48 +0800

    Remove unused global: CORE_NAME
    (bzr r3262.3.73)

--- compizconfig/gsettings/gsettings_backend_shared/gsettings_constants.c
+++ compizconfig/gsettings/gsettings_backend_shared/gsettings_constants.c
@@ -6,6 +6,5 @@ const char * const PROFILE_SCHEMA_ID = "org.freedesktop.compizconfig.profile";
 const char * const COMPIZ       = "/org/freedesktop/compiz";
 const char * const COMPIZ_PROFILEPATH = "/org/freedesktop/compiz/";
 const char * const DEFAULTPROF = "Default";
-const char * const CORE_NAME   = "core";
 
 const unsigned int MAX_GSETTINGS_KEY_SIZE = 1024;
