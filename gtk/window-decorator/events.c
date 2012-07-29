671b777eda2155cf390759719e4eb0f04b601090
MC Return <mc.return@gmx.net>
2012-07-30 01:19:35 +0200

    Reduced the scope of the variables 'last_button_num',  'last_button_x' and 'last_button_y'
    (bzr r3294.4.105)

--- gtk/window-decorator/events.c
+++ gtk/window-decorator/events.c
@@ -440,11 +440,8 @@ title_event (WnckWindow       *win,
 	     decor_event      *gtkwd_event,
 	     decor_event_type gtkwd_type)
 {
-    static int	  last_button_num = 0;
     static Window last_button_xwindow = None;
     static Time	  last_button_time = 0;
-    static int	  last_button_x = 0;
-    static int	  last_button_y = 0;
 
     decor_t *d = g_object_get_data (G_OBJECT (win), "decor");
 
@@ -460,6 +457,9 @@ title_event (WnckWindow       *win,
 
     if (gtkwd_event->button == 1)
     {
+    static int last_button_num = 0;
+    static int last_button_x = 0;
+    static int last_button_y = 0;
 	if (gtkwd_event->button == last_button_num		        &&
 	    gtkwd_event->window == last_button_xwindow		        &&
 	    gtkwd_event->time < last_button_time + double_click_timeout &&
