5e84880b387b793b78bf1d2c33e2181fc608a40d
MC Return <mc.return@gmx.net>
2012-07-29 23:52:01 +0200

    Fixed possible performance problem by using prefix ++ operator for rit
    (bzr r3294.4.45)

--- plugins/animation/src/extensionplugin.cpp
+++ plugins/animation/src/extensionplugin.cpp
@@ -227,7 +227,7 @@ ExtensionPluginAnimation::prePreparePaintGeneral ()
     // Now initiate focus animations (after the restack chains are formed
     // right above)
     for (CompWindowVector::reverse_iterator rit = mLastClientList.rbegin ();
-	 rit != mLastClientList.rend (); rit++)
+     rit != mLastClientList.rend (); ++rit)
     {
 	CompWindow *w = (*rit);
 	AnimWindow *aw = AnimWindow::get (w);
