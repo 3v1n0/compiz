51e2ce9e225ba8f2e38e635c032a71e8382a1984
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-08-04 09:20:20 +0800

    Bootstrapped wrapper tests
    (bzr r3279.10.107)

--- compizconfig/gsettings/gsettings_backend_shared/ccs_gsettings_interface_wrapper.c
+++ compizconfig/gsettings/gsettings_backend_shared/ccs_gsettings_interface_wrapper.c
@@ -1,1 +1,13 @@
 #include "ccs_gsettings_interface_wrapper.h"
+
+CCSGSettingsWrapper *
+ccsGSettingsWrapperNewForSchemaWithPath (const char *schema,
+					 const char *path)
+{
+    return NULL;
+}
+
+void
+ccsFreeGSettingsWrapper (CCSGSettingsWrapper *wrapper)
+{
+}
--- compizconfig/gsettings/tests/test_gsettings_wrapper.cpp
+++ compizconfig/gsettings/tests/test_gsettings_wrapper.cpp
@@ -1,28 +1,52 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <boost/shared_ptr.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
 #include "test_gsettings_tests.h"
+#include <gsettings-mock-schemas-config.h>
 #include <ccs_gsettings_interface_wrapper.h>
+
+using ::testing::NotNull;
 
 class TestGSettingsWrapperWithMemoryBackendEnv :
     public ::testing::Test
 {
     public:
 
+	TestGSettingsWrapperWithMemoryBackendEnv () :
+	    mockSchema ("org.compiz.mock"),
+	    mockPath ("/org/compiz/mock/mock")
+	{
+	}
+
 	virtual void SetUp ()
 	{
+	    g_setenv ("GSETTINGS_SCHEMA_DIR", MOCK_PATH.c_str (), true);
 	    g_setenv ("GSETTINGS_BACKEND", "memory", 1);
 	}
 
 	virtual void TearDown ()
 	{
 	    g_unsetenv ("GSETTINGS_BACKEND");
+	    g_unsetenv ("GSETTINGS_SCHEMA_DIR");
 	}
+
+    protected:
+
+	std::string mockSchema;
+	std::string mockPath;
 };
 
 TEST_F (TestGSettingsWrapperWithMemoryBackendEnv, TestWrapperConstruction)
 {
-    FAIL ();
+    boost::shared_ptr <CCSGSettingsWrapper> wrapper (ccsGSettingsWrapperNewForSchemaWithPath (mockSchema.c_str (),
+											      mockPath.c_str ()),
+						     boost::bind (ccsFreeGSettingsWrapper, _1));
+
+    EXPECT_THAT (wrapper.get (), NotNull ());
 }
 
 TEST_F (TestGSettingsWrapperWithMemoryBackendEnv, TestSetValueOnWrapper)
