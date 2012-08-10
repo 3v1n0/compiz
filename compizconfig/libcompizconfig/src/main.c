e8fbc63ede95d066618ae0e5941c55bb497b7da8
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-08-10 12:54:50 +0800

    Don't clobber the backend info we obtain from the plugin and don't free objects in the list as they're just references to internal data
    (bzr r3279.7.49)

--- compizconfig/libcompizconfig/src/main.c
+++ compizconfig/libcompizconfig/src/main.c
@@ -3576,13 +3576,6 @@ addBackendInfo (CCSBackendInfoList * bl, char *file)
 	return;
     }
 
-    info = calloc (1, sizeof (CCSBackendInfo));
-    if (!info)
-    {
-	dlclose (dlhand);
-	return;
-    }
-
     *bl = ccsBackendInfoListAppend (*bl, (CCSBackendInfo *) info);
     dlclose (dlhand);
 }
