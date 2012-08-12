abdabe1fe4163967e5967265649b54041b16007e
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-08-12 19:52:05 +0800

    We can't read backend info when backends are closed ... we need to copy the info out instead
    (bzr r3279.7.63)

--- compizconfig/compizconfig-python/src/compizconfig.pyx
+++ compizconfig/compizconfig-python/src/compizconfig.pyx
@@ -1216,7 +1216,7 @@ cdef class Context:
             self.backends[backendInfo.name] = Backend (self, info)
             backendList = backendList.next
 
-        ccsBackendInfoListFree (origBackendList, False)
+        ccsBackendInfoListFree (origBackendList, True)
 
         self.currentBackend = self.backends[ccsGetBackend (self.ccsContext)]
     
