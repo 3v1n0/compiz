bb2c67ee4214dbc426e4f7044d54ca2d4af5081b
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-08-22 10:07:34 +0800

    Detect if rsvg 2.36.2 is installed
    Fixes LP: #1039843
    
    (bzr r3314.2.1)

--- plugins/imgsvg/src/imgsvg.h
+++ plugins/imgsvg/src/imgsvg.h
@@ -33,8 +33,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <config.h>
+
 #include <cairo/cairo-xlib.h>
 #include <librsvg/rsvg.h>
+
+#if !HAVE_RSVG_2_36_2
+#include <librsvg/rsvg-cairo.h>
+#endif
 
 #include <X11/Xatom.h>
 #include <X11/extensions/shape.h>
