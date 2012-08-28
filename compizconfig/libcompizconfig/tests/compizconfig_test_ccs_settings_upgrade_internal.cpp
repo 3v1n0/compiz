1684661824becd261d93701ae95774aa54bb3718
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-08-28 22:51:21 +0800

    Added a test harness for internal upgrades
    (bzr r3324.2.4)

---
+++ compizconfig/libcompizconfig/tests/compizconfig_test_ccs_settings_upgrade_internal.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "ccs_settings_upgrade_internal.h"
+
+class CCSSettingsUpgradeInternalTest :
+    public ::testing::Test
+{
+};
+
+TEST_F (CCSSettingsUpgradeInternalTest, TestTest)
+{
+}
+
+
