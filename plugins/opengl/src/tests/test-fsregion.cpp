df5483285a50049fd9c6b7c848932ec041b0cd66
Daniel van Vugt <daniel.van.vugt@canonical.com>
2012-09-04 16:00:27 +0800

    Add more tests and fix some search/replace mistakes in test-fsregion.cpp
    (bzr r3323.1.20)

--- plugins/opengl/src/tests/test-fsregion.cpp
+++ plugins/opengl/src/tests/test-fsregion.cpp
@@ -31,8 +31,20 @@ using namespace compiz::opengl;
 TEST (OpenGLFullscreenRegion, NoWindows)
 {
     FullscreenRegion monitor (CompRect (0, 0, 1024, 768));
-    CompRegion desktop (0, 0, 1024, 768);
-    EXPECT_FALSE (monitor.isCoveredBy (desktop, false));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
+}
+
+TEST (OpenGLFullscreenRegion, OneFullscreen)
+{
+    FullscreenRegion monitor (CompRect (0, 0, 1024, 768));
+    EXPECT_TRUE  (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
+}
+
+TEST (OpenGLFullscreenRegion, FullscreenNoDesktop)
+{
+    FullscreenRegion monitor (CompRect (0, 0, 1024, 768));
+    EXPECT_TRUE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
 }
 
 TEST (OpenGLFullscreenRegion, NormalWindows)
@@ -40,7 +52,7 @@ TEST (OpenGLFullscreenRegion, NormalWindows)
     FullscreenRegion monitor (CompRect (0, 0, 1024, 768));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (10, 10, 40, 30)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (20, 20, 50, 20)));
-    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
 }
 
 TEST (OpenGLFullscreenRegion, TwoFullscreen)
@@ -50,7 +62,7 @@ TEST (OpenGLFullscreenRegion, TwoFullscreen)
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (10, 10, 40, 30)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (20, 20, 50, 20)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
-    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
 }
 
 TEST (OpenGLFullscreenRegion, Offscreen)
@@ -62,7 +74,7 @@ TEST (OpenGLFullscreenRegion, Offscreen)
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (10, 10, 40, 30)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (20, 20, 50, 20)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
-    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
 }
 
 TEST (OpenGLFullscreenRegion, CancelFullscreen1)
@@ -73,7 +85,7 @@ TEST (OpenGLFullscreenRegion, CancelFullscreen1)
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (10, 10, 40, 30)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (20, 20, 50, 20)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
-    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
 }
 
 TEST (OpenGLFullscreenRegion, CancelFullscreen2)
@@ -86,7 +98,7 @@ TEST (OpenGLFullscreenRegion, CancelFullscreen2)
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (10, 10, 40, 30)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (20, 20, 50, 20)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
-    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
 }
 
 TEST (OpenGLFullscreenRegion, Overflow)
@@ -95,6 +107,7 @@ TEST (OpenGLFullscreenRegion, Overflow)
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (10, 10, 40, 30)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (-10, -10, 1044, 788)));
     EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768)));
+    EXPECT_FALSE (monitor.isCoveredBy (CompRegion (0, 0, 1024, 768), false));
 }
 
 
