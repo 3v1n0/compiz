adf465dbbcc99b1dbb34b231fdcfb05f9129506d
Sam Spilsbury <sam.spilsbury@canonical.com>
2012-09-07 09:18:53 +0800

    No need to disambiguate std
    (bzr r3324.3.37)

--- compizconfig/tests/compizconfig_ccs_setting_value_operators.h
+++ compizconfig/tests/compizconfig_ccs_setting_value_operators.h
@@ -34,8 +34,8 @@ operator== (const CCSSettingColorValue &lhs,
     return false;
 }
 
-::std::ostream &
-operator<< (::std::ostream &os, const CCSSettingColorValue &v)
+std::ostream &
+operator<< (std::ostream &os, const CCSSettingColorValue &v)
 {
     return os << "Red: " << std::hex << v.color.red << "Blue: " << std::hex << v.color.blue << "Green: " << v.color.green << "Alpha: " << v.color.alpha
        << std::dec << std::endl;
@@ -50,8 +50,8 @@ operator== (const CCSSettingKeyValue &lhs,
     return false;
 }
 
-::std::ostream &
-operator<< (::std::ostream &os, const CCSSettingKeyValue &v)
+std::ostream &
+operator<< (std::ostream &os, const CCSSettingKeyValue &v)
 {
     return os << "Keysym: " << v.keysym << " KeyModMask " << std::hex << v.keyModMask << std::dec << std::endl;
 }
@@ -65,8 +65,8 @@ operator== (const CCSSettingButtonValue &lhs,
     return false;
 }
 
-::std::ostream &
-operator<< (::std::ostream &os, const CCSSettingButtonValue &v)
+std::ostream &
+operator<< (std::ostream &os, const CCSSettingButtonValue &v)
 {
     return os << "Button " << v.button << "Button Key Mask: " << std::hex << v.buttonModMask << "Edge Mask: " << v.edgeMask << std::dec << std::endl;
 }
@@ -95,8 +95,8 @@ operator== (const std::string &rhs,
     return *lhs == rhs;
 }
 
-::std::ostream &
-operator<< (::std::ostream &os, CCSString &string)
+std::ostream &
+operator<< (std::ostream &os, CCSString &string)
 {
     os << string.value << std::endl;
     return os;
