aab444e1fdf9f7d105657818286e04ede60b197b
Sampo555 <sampo_75@windowslive.com>
2012-10-13 12:33:43 +0300

    Problem: Specific Zoom Factor settings were ignored by the Enhanced Zoom Desktop (LP: #1066187) because the values were only read once during initial load of the plugin probably before the user defined configuration was read (and thus it always used the default values).
    Fix:
    Instead of sending the (original) value of the initiated zoom level, send a zoom level identifier. The identifier is used to determine which zoom level the user wants and a fresh setting for that zoom level is used.
    
    Fixes LP: #1066187
    
    (bzr r3414.1.1)

--- plugins/ezoom/src/ezoom.h
+++ plugins/ezoom/src/ezoom.h
@@ -49,6 +49,12 @@
 
 #include <cmath>
 
+enum SpecificZoomTarget {
+    ZoomTargetFirst = 0,
+    ZoomTargetSecond,
+    ZoomTargetThird
+};
+
 class EZoomScreen :
     public PluginClassHandler <EZoomScreen, CompScreen>,
     public EzoomOptions,
@@ -314,7 +320,7 @@ class EZoomScreen :
 	zoomSpecific (CompAction         *action,
 		      CompAction::State  state,
 		      CompOption::Vector options,
-		      float		     target);
+		      SpecificZoomTarget target);
 
 	bool
 	zoomToWindow (CompAction         *action,
