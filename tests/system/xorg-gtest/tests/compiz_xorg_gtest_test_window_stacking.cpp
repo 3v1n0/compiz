7f3262752bf8a0a1dacfb2920e139fd820ac028a
Andrea Azzarone <azzaronea@gmail.com>
2012-11-28 18:59:10 +0100

    Add tests.
    (bzr r3485.1.4)

--- tests/system/xorg-gtest/tests/compiz_xorg_gtest_test_window_stacking.cpp
+++ tests/system/xorg-gtest/tests/compiz_xorg_gtest_test_window_stacking.cpp
@@ -91,6 +91,27 @@ namespace
 	return w;
     }
 
+    void SetUserTime (Display *dpy, Window w, Time time)
+    {
+	Atom _NET_WM_USER_TIME = XInternAtom (dpy, "_NET_WM_USER_TIME", false);
+	unsigned int value = (unsigned int) time;
+
+	XChangeProperty (dpy, w,
+			 _NET_WM_USER_TIME,
+			 XA_CARDINAL, 32, PropModeReplace,
+			 unsigned char *) &value, 1);
+    }
+
+    void SetClientLeader (Display *dpy, Window w, Window leader)
+    {
+	Atom WM_CLIENT_LEADER = XInternAtom (dpy, "WM_CLIENT_LEADER", false);
+
+	XChangeProperty (dpy, w,
+			 WM_CLIENT_LEADER,
+			 XA_WINDOW, 32, PropModeReplace,
+			 (unsigned char *) &leader, 1);
+    }
+
     class PropertyNotifyXEventMatcher :
 	public ct::XEventMatcher
     {
@@ -214,4 +235,70 @@ TEST_F (CompizXorgSystemStackingTest, TestCreateWindowsAndRestackRelativeToEachO
     EXPECT_EQ (*it++, w1); /* first should be the bottom normal window */
     EXPECT_EQ (*it++, w2); /* second should be the top normal window */
     EXPECT_EQ (*it++, dock); /* dock must always be on top */
+}
+
+TEST_F (CompizXorgSystemStackingTest, TestMapWindowWithOldUserTime)
+{
+    ::Display *dpy = Display ();
+    PropertyNotifyXEventMatcher matcher (dpy, "_NET_CLIENT_LIST_STACKING");
+
+    Window w1 = CreateNormalWindow (dpy);
+    Window w2 = CreateNormalWindow (dpy);
+    Window w3 = CreateNormalWindow (dpy);
+
+    XMapRaised (dpy, w1);
+    XMapRaised (dpy, w2);
+
+    /* Wait for property change notify on the root window to happen twice */
+    ASSERT_TRUE (ct::WaitForEventOfTypeOnWindowMatching (dpy, DefaultRootWindow (dpy), PropertyNotify, -1, -1, matcher));
+    ASSERT_TRUE (ct::WaitForEventOfTypeOnWindowMatching (dpy, DefaultRootWindow (dpy), PropertyNotify, -1, -1, matcher));
+
+    SetUserTime (dpy, w2, 200);
+    SetClientLeader (dpy, w2, w2);
+    SetUserTime (dpy, w3, 100);
+    SetClientLeader (dpy, w3, w3);
+
+    XMapRaised (dpy, w3);
+    ASSERT_TRUE (ct::WaitForEventOfTypeOnWindowMatching (dpy, DefaultRootWindow (dpy), PropertyNotify, -1, -1, matcher));
+
+    /* Check the client list to see that w2 > w3 > w1 */
+    std::list <Window> clientList = ct::NET_CLIENT_LIST_STACKING (dpy);
+    ASSERT_EQ (clientList.size (), 3);
+
+    std::list <Window>::iterator it = clientList.begin ();
+    EXPECT_EQ (*it++, w1);
+    EXPECT_EQ (*it++, w3);
+    EXPECT_EQ (*it++, w2);
+}
+
+TEST_F (CompizXorgSystemStackingTest, TestMapWindowAndDenyFocus)
+{
+    ::Display *dpy = Display ();
+    PropertyNotifyXEventMatcher matcher (dpy, "_NET_CLIENT_LIST_STACKING");
+
+    Window w1 = CreateNormalWindow (dpy);
+    Window w2 = CreateNormalWindow (dpy);
+    Window w3 = CreateNormalWindow (dpy);
+
+    XMapRaised (dpy, w1);
+    XMapRaised (dpy, w2);
+
+    /* Wait for property change notify on the root window to happen twice */
+    ASSERT_TRUE (ct::WaitForEventOfTypeOnWindowMatching (dpy, DefaultRootWindow (dpy), PropertyNotify, -1, -1, matcher));
+    ASSERT_TRUE (ct::WaitForEventOfTypeOnWindowMatching (dpy, DefaultRootWindow (dpy), PropertyNotify, -1, -1, matcher));
+
+    SetUserTime (dpy, w3, 0);
+    SetClientLeader (dpy, w3, w3);
+
+    XMapRaised (dpy, w3);
+    ASSERT_TRUE (ct::WaitForEventOfTypeOnWindowMatching (dpy, DefaultRootWindow (dpy), PropertyNotify, -1, -1, matcher));
+
+    /* Check the client list to see that w2 > w3 > w1 */
+    std::list <Window> clientList = ct::NET_CLIENT_LIST_STACKING (dpy);
+    ASSERT_EQ (clientList.size (), 3);
+
+    std::list <Window>::iterator it = clientList.begin ();
+    EXPECT_EQ (*it++, w1);
+    EXPECT_EQ (*it++, w3);
+    EXPECT_EQ (*it++, w2);
 }
