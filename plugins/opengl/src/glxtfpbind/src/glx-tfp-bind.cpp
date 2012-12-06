faaae5a9b847604d9cf6dfc763cfabb29f8caeb1
Daniel van Vugt <daniel.van.vugt@canonical.com>
2012-12-06 13:06:15 +0800

    Clarify the reasoning behind what might look light pointless code to the unexpecting reader. Also make it conditional, because the need for the fix is conditional on the same macro.
    (bzr r3500.3.2)

--- plugins/opengl/src/glxtfpbind/src/glx-tfp-bind.cpp
+++ plugins/opengl/src/glxtfpbind/src/glx-tfp-bind.cpp
@@ -53,8 +53,11 @@ cgl::bindTexImageGLX (ServerGrabInterface                *serverGrabInterface,
 	if (!checkPixmapValidity (x11Pixmap))
 	    return false;
 
+#ifdef LP_1030891_NOT_FIXED
+	// We need to bind before the above ServerLock is lost
 	bindTexImageEXT (glxPixmap);
 	return true;
+#endif
     }
 
     bindTexImageEXT (glxPixmap);
