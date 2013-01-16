66b224ba276c34a017d00f3c9f8ad95ebb15d49c
Sam Spilsbury <sam.spilsbury@canonical.com>
2013-01-16 09:48:25 +0800

    Update copyrights
    (bzr r3554.4.5)

--- tests/system/xorg-gtest/tests/compiz_xorg_gtest_ewmh.cpp
+++ tests/system/xorg-gtest/tests/compiz_xorg_gtest_ewmh.cpp
@@ -1,7 +1,7 @@
 /*
  * Compiz XOrg GTest, EWMH compliance
  *
- * Copyright (C) 2012 Canonical Ltd.
+ * Copyright (C) 2013 Sam Spilsbury
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
