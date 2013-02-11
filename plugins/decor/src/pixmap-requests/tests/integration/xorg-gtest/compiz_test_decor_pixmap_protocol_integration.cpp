20f5845f8317d628f1273d73c0c2f08068aa4822
Sam Spilsbury <sam.spilsbury@canonical.com>
2013-02-11 17:48:58 +0800

    Expect that calling decor_post_delete_message gets us a ClientMessage
    (bzr r3602.2.16)

--- plugins/decor/src/pixmap-requests/tests/integration/xorg-gtest/compiz_test_decor_pixmap_protocol_integration.cpp
+++ plugins/decor/src/pixmap-requests/tests/integration/xorg-gtest/compiz_test_decor_pixmap_protocol_integration.cpp
@@ -35,18 +35,132 @@
 #include "decoration.h"
 
 #include <xorg/gtest/xorg-gtest.h>
+#include <compiz-xorg-gtest.h>
 
 #include "pixmap-requests.h"
 
+namespace xt = xorg::testing;
+namespace ct = compiz::testing;
 namespace cd = compiz::decor;
 namespace cdp = compiz::decor::protocol;
+
+using ::testing::MatcherInterface;
+using ::testing::MatchResultListener;
+using ::testing::_;
 
 class DecorPixmapProtocol :
     public xorg::testing::Test
 {
     public:
+
+	typedef boost::function <void (const XClientMessageEvent &)> ClientMessageReceiver;
+
+	void SetUp ();
+
+	void WaitForClientMessageOnAndDeliverTo (Window client,
+						 Atom   message,
+						 const ClientMessageReceiver &receiver);
+
+    protected:
+
+	Atom deletePixmapMessage;
 };
 
-TEST_F (DecorPixmapProtocol, Check)
+namespace
 {
+bool Advance (Display *d, bool result)
+{
+    return ct::AdvanceToNextEventOnSuccess (d, result);
+}
+
+Window MOCK_WINDOW = 1;
+Pixmap MOCK_PIXMAP = 2;
+
+class MockClientMessageReceiver
+{
+    public:
+
+	MOCK_METHOD1 (receiveMsg, void (const XClientMessageEvent &));
+};
+}
+
+void
+DecorPixmapProtocol::SetUp ()
+{
+    xorg::testing::Test::SetUp ();
+
+    XSelectInput (Display (),
+		  DefaultRootWindow (Display ()),
+		  StructureNotifyMask);
+
+    deletePixmapMessage = XInternAtom (Display (), DECOR_DELETE_PIXMAP_ATOM_NAME, 0);
+}
+
+class DeliveryMatcher :
+    public ct::XEventMatcher
+{
+    public:
+
+	DeliveryMatcher (Atom message,
+			 const DecorPixmapProtocol::ClientMessageReceiver &receiver) :
+	    mMessage (message),
+	    mReceiver (receiver)
+	{
+	}
+
+	bool MatchAndExplain (const XEvent &xce,
+			      MatchResultListener *listener) const
+	{
+	    if (xce.xclient.message_type == mMessage)
+	    {
+		mReceiver (reinterpret_cast <const XClientMessageEvent &> (xce));
+		return true;
+	    }
+	    else
+		return false;
+	}
+
+	void DescribeTo (std::ostream *os) const
+	{
+	    *os << "Message delivered";
+	}
+
+    private:
+
+	Atom mMessage;
+	DecorPixmapProtocol::ClientMessageReceiver mReceiver;
+};
+
+void
+DecorPixmapProtocol::WaitForClientMessageOnAndDeliverTo (Window client,
+							 Atom   message,
+							 const ClientMessageReceiver &receiver)
+{
+    ::Display *d = Display ();
+
+    DeliveryMatcher delivery (message, receiver);
+    ASSERT_TRUE (Advance (d, ct::WaitForEventOfTypeOnWindowMatching (d,
+								     client,
+								     ClientMessage,
+								     -1,
+								     -1,
+								     delivery)));
+}
+
+TEST_F (DecorPixmapProtocol, PostDeleteCausesClientMessage)
+{
+    MockClientMessageReceiver receiver;
+    ::Display *d = Display ();
+
+    decor_post_delete_pixmap (d,
+			      MOCK_WINDOW,
+			      MOCK_PIXMAP);
+
+    EXPECT_CALL (receiver, receiveMsg (_)).Times (1);
+
+    WaitForClientMessageOnAndDeliverTo (MOCK_WINDOW,
+					deletePixmapMessage,
+					boost::bind (&MockClientMessageReceiver::receiveMsg,
+						     &receiver,
+						     _1));
 }
