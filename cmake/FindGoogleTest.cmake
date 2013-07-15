863cc6ae305708c0e42e0126d49c33d8c5e49f33
Andrea Azzarone <azzaronea@gmail.com>
2013-07-15 11:48:24 +0200

    Use new gmock.
    (bzr r3759.1.1)

--- cmake/FindGoogleTest.cmake
+++ cmake/FindGoogleTest.cmake
@@ -1,50 +1,37 @@
-# Find Google Test and Google Mock, either with pkg-config or by
-# searching the system install paths. This sets:
-#
-# GOOGLE_TEST_AND_MOCK_FOUND
-# GTEST_INCLUDE_DIR
-# GTEST_ROOT_DIR
-# GMOCK_LIBRARY
-# GMOCK_MAIN_LIBRARY
-
-find_package (GTest)
 find_package (Threads REQUIRED)
 
-if (NOT GTEST_FOUND)
+set (GMOCK_INCLUDE_DIR "/usr/include/gmock/include" CACHE PATH "gmock source include directory")
+set (GMOCK_SOURCE_DIR "/usr/src/gmock" CACHE PATH "gmock source directory")
+set (GTEST_INCLUDE_DIR "${GMOCK_SOURCE_DIR}/gtest/include" CACHE PATH "gtest source include directory")
 
-    # Check for google test and build it locally
-    set (GTEST_ROOT_DIR
- 	 "/usr/src/gtest" # Default value, adjustable by user with e.g., ccmake
-	 CACHE
-	 PATH
-	 "Path to Google Test srcs"
-	 FORCE)
+add_subdirectory(${GMOCK_SOURCE_DIR} "${CMAKE_BINARY_DIR}/__gmock")
 
-    find_path (GTEST_INCLUDE_DIR gtest/gtest.h)
+include_directories (${GMOCK_INCLUDE_DIR} ${GTEST_INCLUDE_DIR})
 
-    set (GTEST_BOTH_LIBRARIES gtest gtest_main ${CMAKE_THREAD_LIBS_INIT})
+set (GTEST_BOTH_LIBRARIES ${CMAKE_THREAD_LIBS_INIT} gtest gtest_main)
+
+if (NOT GTEST_BOTH_LIBRARIES)
+
+    message ("Google Test not found - cannot build tests!")
+    set (GTEST_FOUND FALSE)
+
+else (NOT GTEST_BOTH_LIBRARIES)
+
     set (GTEST_FOUND TRUE)
-    set (GTEST_LOCAL_BUILD_REQUIRED TRUE)
 
-else (NOT GTEST_FOUND)
+endif (NOT GTEST_BOTH_LIBRARIES)
 
-    set (GTEST_LOCAL_BUILD_REQUIRED FALSE)
-
-    # We found gtest. Append CMAKE_THREAD_LIBS_INIT to the provided libraries.
-    set (GTEST_BOTH_LIBRARIES ${GTEST_BOTH_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
-
-endif (NOT GTEST_FOUND)
-
-find_library (GMOCK_LIBRARY gmock)
-find_library (GMOCK_MAIN_LIBRARY gmock_main)
+set (GMOCK_LIBRARY gmock)
+set (GMOCK_MAIN_LIBRARY gmock_main)
+add_definitions(-DGTEST_USE_OWN_TR1_TUPLE=0)
 
 if (NOT GMOCK_LIBRARY OR NOT GMOCK_MAIN_LIBRARY OR NOT GTEST_FOUND)
 
     message ("Google Mock and Google Test not found - cannot build tests!")
-    set (GOOGLE_TEST_AND_MOCK_FOUND OFF CACHE BOOL "" FORCE)
+    set (GOOGLE_TEST_AND_MOCK_FOUND FALSE)
 
 else (NOT GMOCK_LIBRARY OR NOT GMOCK_MAIN_LIBRARY OR NOT GTEST_FOUND)
 
-    set (GOOGLE_TEST_AND_MOCK_FOUND ON CACHE BOOL "" FORCE)
+    set (GOOGLE_TEST_AND_MOCK_FOUND TRUE)
 
 endif (NOT GMOCK_LIBRARY OR NOT GMOCK_MAIN_LIBRARY OR NOT GTEST_FOUND)
