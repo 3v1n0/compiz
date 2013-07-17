4a8e41ae9107c8b89dcc18a4000b9d31e8684b26
Sam Spilsbury <sam.spilsbury@canonical.com>
2013-07-17 00:50:48 -0700

    Remove duplication and fix wrong order for set ()
    (bzr r3753.6.8)

--- cmake/GoogleTest.cmake
+++ cmake/GoogleTest.cmake
@@ -20,12 +20,12 @@ if (GMOCK_INCLUDE_DIR)
             ${GMOCK_INCLUDE_PREFIX_LENGTH}
             GMOCK_INCLUDE_PREFIX)
 
-    set (GMOCK_SRC_DIR CACHE PATH "Path to Google Mock Sources" ${GMOCK_INCLUDE_PREFIX}/src/gmock)
-    set (GMOCK_INCLUDE_DIR CACHE PATH "Path to Google Mock Headers" ${GMOCK_INCLUDE_DIR})
-    set (GTEST_INCLUDE_DIR CACHE PATH "Path to Google Test Headers" ${GMOCK_SRC_DIR}/gtest/include)
+    set (GMOCK_SRC_DIR ${GMOCK_INCLUDE_PREFIX}/src/gmock CACHE PATH "Path to Google Mock Sources")
+    set (GMOCK_INCLUDE_DIR ${GMOCK_INCLUDE_DIR} CACHE PATH "Path to Google Mock Headers")
+    set (GTEST_INCLUDE_DIR ${GMOCK_SRC_DIR}/gtest/include CACHE PATH "Path to Google Test Headers")
 
-    set (GMOCK_LIBRARY CACHE STRING "Name of the Google Mock library" gmock)
-    set (GMOCK_MAIN_LIBRARY CACHE STIRNG "Name of the Google Mock main () library" gmock_main)
+    set (GMOCK_LIBRARY "gmock" CACHE STRING "Name of the Google Mock library")
+    set (GMOCK_MAIN_LIBRARY "gmock_main" CACHE STIRNG "Name of the Google Mock main () library")
     set (GTEST_BOTH_LIBRARIES ${CMAKE_THREAD_LIBS_INIT} gtest gtest_main)
 
 endif (GMOCK_INCLUDE_DIR)
@@ -50,7 +50,7 @@ else (NOT GMOCK_LIBRARY OR NOT GMOCK_MAIN_LIBRARY OR NOT GTEST_FOUND)
     set (GOOGLE_TEST_AND_MOCK_FOUND TRUE)
 
     add_definitions(-DGTEST_USE_OWN_TR1_TUPLE=0)
-    add_subdirectory (${GMOCK_SOURCE_DIR} "${CMAKE_BINARY_DIR}/__gmock")
+    add_subdirectory (${GMOCK_SRC_DIR} ${CMAKE_BINARY_DIR}/__gmock)
     include_directories (${GMOCK_INCLUDE_DIR}
                          ${GTEST_INCLUDE_DIR})
 
