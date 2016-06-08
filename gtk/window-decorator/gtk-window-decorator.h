fc540390690235568529936ae7a862f1a4ba66b3
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2016-06-08 13:34:32 +0300

    Move decor_update_switcher_property to switcher.c.
    (bzr r4052.1.6)

--- gtk/window-decorator/gtk-window-decorator.h
+++ gtk/window-decorator/gtk-window-decorator.h
@@ -440,11 +440,6 @@ decor_update_blur_property (decor_t *d,
 			    Region  right_region,
 			    int     right_offset);
 
-/* decorprops.c */
-
-void
-decor_update_switcher_property (decor_t *d);
-
 /* cairo.c */
 
 #define CORNER_TOPLEFT     (1 << 0)
--- gtk/window-decorator/switcher.c
+++ gtk/window-decorator/switcher.c
@@ -27,6 +27,61 @@
 
 #define SWITCHER_SPACE 40
 
+static void
+decor_update_switcher_property (decor_t *d)
+{
+    long	 *data;
+    Display	 *xdisplay = GDK_DISPLAY_XDISPLAY (gdk_display_get_default ());
+    gint	 nQuad;
+    decor_quad_t quads[N_QUADS_MAX];
+    unsigned int    nOffset = 1;
+    unsigned int   frame_type = populate_frame_type (d);
+    unsigned int   frame_state = populate_frame_state (d);
+    unsigned int   frame_actions = populate_frame_actions (d);
+    GtkStyleContext *context;
+    GdkRGBA fg;
+    long         fgColor[4];
+    
+    nQuad = decor_set_lSrStSbX_window_quads (quads, &d->frame->window_context_active,
+					     &d->border_layout,
+					     d->border_layout.top.x2 -
+					     d->border_layout.top.x1 -
+					     d->frame->window_context_active.extents.left -
+						 d->frame->window_context_active.extents.right -
+						     32);
+    
+    data = decor_alloc_property (nOffset, WINDOW_DECORATION_TYPE_PIXMAP);
+    decor_quads_to_property (data, nOffset - 1, cairo_xlib_surface_get_drawable (d->surface),
+			     &d->frame->win_extents, &d->frame->win_extents,
+			     &d->frame->win_extents, &d->frame->win_extents,
+			     0, 0, quads, nQuad, frame_type, frame_state, frame_actions);
+
+    context = gtk_widget_get_style_context (d->frame->style_window_rgba);
+
+    gtk_style_context_save (context);
+    gtk_style_context_set_state (context, GTK_STATE_FLAG_NORMAL);
+    gtk_style_context_get_color (context, GTK_STATE_FLAG_NORMAL, &fg);
+    gtk_style_context_restore (context);
+
+    fgColor[0] = fg.red;
+    fgColor[1] = fg.green;
+    fgColor[2] = fg.blue;
+    fgColor[3] = SWITCHER_ALPHA;
+    
+    gdk_error_trap_push ();
+    XChangeProperty (xdisplay, d->prop_xid,
+		     win_decor_atom,
+		     XA_INTEGER,
+		     32, PropModeReplace, (guchar *) data,
+		     PROP_HEADER_SIZE + BASE_PROP_SIZE + QUAD_PROP_SIZE * N_QUADS_MAX);
+    XChangeProperty (xdisplay, d->prop_xid, switcher_fg_atom,
+		     XA_INTEGER, 32, PropModeReplace, (guchar *) fgColor, 4);
+    gdk_display_sync (gdk_display_get_default ());
+    gdk_error_trap_pop_ignored ();
+
+    free (data);
+}
+
 decor_frame_t *
 create_switcher_frame (const gchar *type)
 {
