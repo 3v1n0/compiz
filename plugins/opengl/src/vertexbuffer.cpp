948b3307a87b0c79343869c4656eb1956b187617
Sam Spilsbury smspillaz@gmail.com <>
2016-07-10 07:57:56 +0800

    Unmerge simple-animations addition
    (bzr r4067.2.4)

--- plugins/opengl/src/vertexbuffer.cpp
+++ plugins/opengl/src/vertexbuffer.cpp
@@ -678,12 +678,11 @@ int PrivateVertexBuffer::legacyRender (const GLMatrix            &projection,
 			  maxVertices) :
 		vertexData.size () / 3;
 
-    if (indices && nIndices) {
+    if (indices && nIndices)
 	glDrawElements (primitiveType,
 			nIndices,
 			GL_UNSIGNED_SHORT,
 			indices);
-    }
     else
  	glDrawArrays (primitiveType, vertexOffset, nVerticesToDraw);
 
