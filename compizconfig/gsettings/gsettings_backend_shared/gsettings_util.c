672b96d8363524a8a5fc47418bee51831ceb21d8
Andrea Azzarone <azzaronea@gmail.com>
2016-07-26 17:07:59 +0200

    Fix typo.
    (bzr r4071.4.4)

--- compizconfig/gsettings/gsettings_backend_shared/gsettings_util.c
+++ compizconfig/gsettings/gsettings_backend_shared/gsettings_util.c
@@ -458,7 +458,7 @@ checkReadVariantIsValid (GVariant *gsettingsValue, CCSSettingType type, const gc
     if (!gsettingsValue)
     {
 	ccsWarning ("There is no key '%s' at the path %s. "
-		"Settings from this path won't be read. efault value will be used."
+		"Settings from this path won't be read. Default value will be used."
 		"Ensure this setting is available or the setting backend is properly configured.",
 		key, pathName);
 	return FALSE;
