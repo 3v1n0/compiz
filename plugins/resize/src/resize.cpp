7d2f64bf80a1c51faca9dfdc56302ae88538f322
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-06-30 03:00:40 +0200

    Resize: add options to use averageColor for dawing shapes
    If the average color for the desktop is defined, let's use it
    for painting the alternative view of move window.
    Added also an option (on by default) to draw a larger border
    using a lighter or darker color (depending on the current color
    average), to make all this more visible.
    
    (bzr r4121.2.16)

--- plugins/resize/src/resize.cpp
+++ plugins/resize/src/resize.cpp
@@ -70,8 +70,8 @@ resizeInitiateDefaultMode (CompAction	      *action,
 
 static bool
 resizeTerminate (CompAction         *action,
-	         CompAction::State  state,
-	         CompOption::Vector &options)
+		 CompAction::State  state,
+		 CompOption::Vector &options)
 {
     RESIZE_SCREEN (screen);
     return rs->logic.terminateResize(action, state, options);
@@ -94,7 +94,22 @@ ResizeScreen::glPaintRectangle (const GLScreenPaintAttrib &sAttrib,
     GLfloat         vertexData [12];
     GLfloat         vertexData2[24];
     GLint    	   origSrc, origDst;
-    GLushort	    fc[4], bc[4];
+    GLushort	    fc[4], bc[4], averageFillColor[4];
+
+    if (optionGetUseDesktopAverageColor ())
+    {
+	const unsigned short *averageColor = screen->averageColor ();
+
+	if (averageColor)
+	{
+	    borderColor = const_cast<unsigned short *>(averageColor);
+	    memcpy (averageFillColor, averageColor, 4 * sizeof (unsigned short));
+	    averageFillColor[3] = MaxUShort * 0.6;
+
+	    if (fillColor)
+	        fillColor = averageFillColor;
+	}
+    }
 
     bool blend = !optionGetDisableBlend ();
 
@@ -117,12 +132,6 @@ ResizeScreen::glPaintRectangle (const GLScreenPaintAttrib &sAttrib,
 	glGetIntegerv (GL_BLEND_DST, &origDst);
 #endif
     }
-
-    /* Premultiply the alpha values */
-    bc[3] = blend ? ((float) borderColor[3] / MaxUShortFloat) : MaxUShortFloat;
-    bc[0] = ((float) borderColor[0] / MaxUShortFloat) * bc[3];
-    bc[1] = ((float) borderColor[1] / MaxUShortFloat) * bc[3];
-    bc[2] = ((float) borderColor[2] / MaxUShortFloat) * bc[3];
 
     logic.getPaintRectangle (&box);
 
@@ -195,10 +204,42 @@ ResizeScreen::glPaintRectangle (const GLScreenPaintAttrib &sAttrib,
     }
 
     /* draw outline */
-    static const int borderWidth = 2;
-    glLineWidth (borderWidth);
+    static const int defaultBorderWidth = 2;
+    int borderWidth = defaultBorderWidth;
+
+    if (optionGetIncreaseBorderContrast ())
+    {
+	// Generate a lighter color based on border to create more contrast
+	unsigned int averageColorLevel = (borderColor[0] + borderColor[1] + borderColor[2]) / 3;
+	borderWidth *= 2;
+
+	float colorMultiplier;
+	if (averageColorLevel > MaxUShort * 0.3)
+	    colorMultiplier = 0.5; // make it darker
+	else
+	    colorMultiplier = 4.0; // make it lighter
+
+	bc[3] = borderColor[3];
+	bc[0] = MIN(MaxUShortFloat, ((float) borderColor[0]) * colorMultiplier) * bc[3] / MaxUShortFloat;
+	bc[1] = MIN(MaxUShortFloat, ((float) borderColor[1]) * colorMultiplier) * bc[3] / MaxUShortFloat;
+	bc[2] = MIN(MaxUShortFloat, ((float) borderColor[2]) * colorMultiplier) * bc[3] / MaxUShortFloat;
+
+	glLineWidth (borderWidth);
+	streamingBuffer->begin (GL_LINES);
+	streamingBuffer->addVertices (8, &vertexData2[0]);
+	streamingBuffer->addColors (1, bc);
+	streamingBuffer->end ();
+	streamingBuffer->render (sTransform);
+    }
+
+    bc[3] = blend ? borderColor[3] : MaxUShortFloat;
+    bc[0] = borderColor[0] * bc[3] / MaxUShortFloat;
+    bc[1] = borderColor[1] * bc[3] / MaxUShortFloat;
+    bc[2] = borderColor[2] * bc[3] / MaxUShortFloat;
+
+    glLineWidth (defaultBorderWidth);
     streamingBuffer->begin (GL_LINES);
-    streamingBuffer->addColors (1, borderColor);
+    streamingBuffer->addColors (1, bc);
     streamingBuffer->addVertices (8, &vertexData2[0]);
     streamingBuffer->end ();
     streamingBuffer->render (sTransform);
@@ -222,21 +263,21 @@ ResizeScreen::glPaintRectangle (const GLScreenPaintAttrib &sAttrib,
 	damageRegion += CompRect (box.x1 - borderWidth,
 				  box.y1 - borderWidth,
 				  box.x2 - box.x1 + borderWidth * 2,
-				  borderWidth + 1);
+				  borderWidth + borderWidth / 2);
 	// Right
 	damageRegion += CompRect (box.x2 - borderWidth,
 				  box.y1 - borderWidth,
-				  borderWidth + 1,
+				  borderWidth + borderWidth / 2,
 				  box.y2 - box.y1 + borderWidth * 2);
 	// Bottom
 	damageRegion += CompRect (box.x1 - borderWidth,
 				  box.y2 - borderWidth,
 				  box.x2 - box.x1 + borderWidth * 2,
-				  borderWidth + 1);
+				  borderWidth + borderWidth / 2);
 	// Left
 	damageRegion += CompRect (box.x1 - borderWidth,
 				  box.y1 - borderWidth,
-				  borderWidth + 1,
+				  borderWidth + borderWidth / 2,
 				  box.y2 - box.y1 + borderWidth * 2);
     }
     else
